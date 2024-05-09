b21f541565aa9c37bad2a85d9df684cc4c7293d2
kevin.watson <kevindwatson@gmail.com>
2024-05-09 14:16:11 +0100

    first attempt - bugs

--- dash.py
+++ dash.py
@@ -1,7 +1,7 @@
 # dash by Kevin Watson python
 # written 19/01/24 from Thu Vu - https://www.youtube.com/watch?v=uhxiXOTKzfs 4.28
 
-#To change the selected interpreter open the Command Palette using Cmd + Shift + P on Mac or Ctrl + Shift + P on Windows. Then type “Python: Select Interpreter”.
+# To change the selected interpreter open the Command Palette using Cmd + Shift + P on Mac or Ctrl + Shift + P on Windows. Then type “Python: Select Interpreter”.
 
 # git config user.name kevin.watson
 # git config user.email kevindwatson@gmail.com
@@ -20,5 +20,114 @@ import hvplot.pandas
 #df = pd.read_csv('https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv')
 df = pd.read_csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv")
 
-print(df)
+# data processing, replace na with 0 and create derived variable
+df = df.fillna(0)
+df['gdp_per_capita'] = np.where(['population']!=0, df['gdp']/ df['population'], 0)
 
+#print(df)
+
+# make DataFrame Pipeline Interactive
+idf = df.interactive()
+
+# define panel widgets
+year_slider = pn.widgets.IntSlider(name='Year slider', start = 1750, end = 2020, step = 5, value = 1850)
+year_slider
+
+# Radio buttons for CO2 measures
+yaxis_co2 = pn.widgets.RadioButtonGroup(
+    name='Y axis', 
+    options=['co2', 'co2_per_capita',],
+    button_type='success'
+)
+
+continents = ['World', 'Asia', 'Oceania', 'Europe', 'Africa', 'North America', 'South America', 'Antarctica']
+
+co2_pipeline = (
+    idf[
+        (idf.year <= year_slider) &
+        (idf.country.isin(continents))
+    ]
+    .groupby(['country', 'year'])[yaxis_co2].mean()
+    .to_frame()
+    .reset_index()
+    .sort_values(by='year')  
+    .reset_index(drop=True)
+)
+
+co2_pipeline
+
+co2_plot = co2_pipeline.hvplot(x = 'year', by='country', y=yaxis_co2,line_width=2, title="CO2 emission by continent")
+co2_plot
+
+co2_table = co2_pipeline.pipe(pn.widgets.Tabulator, pagination='remote', page_size = 10, sizing_mode='stretch_width') 
+co2_table
+
+co2_vs_gdp_scatterplot_pipeline = (
+    idf[
+        (idf.year == year_slider) &
+        (~ (idf.country.isin(continents)))
+    ]
+    .groupby(['country', 'year', 'gdp_per_capita'])['co2'].mean()
+    .to_frame()
+    .reset_index()
+    .sort_values(by='year')  
+    .reset_index(drop=True)
+)
+
+co2_vs_gdp_scatterplot_pipeline
+
+co2_vs_gdp_scatterplot = co2_vs_gdp_scatterplot_pipeline.hvplot(x='gdp_per_capita', 
+                                                                y='co2', 
+                                                                by='country', 
+                                                                size=80, kind="scatter", 
+                                                                alpha=0.7,
+                                                                legend=False, 
+                                                                height=500, 
+                                                                width=500)
+co2_vs_gdp_scatterplot
+
+yaxis_co2_source = pn.widgets.RadioButtonGroup(
+    name='Y axis', 
+    options=['coal_co2', 'oil_co2', 'gas_co2'], 
+    button_type='success'
+)
+
+continents_excl_world = ['Asia', 'Oceania', 'Europe', 'Africa', 'North America', 'South America', 'Antarctica']
+
+co2_source_bar_pipeline = (
+    idf[
+        (idf.year == year_slider) &
+        (idf.country.isin(continents_excl_world))
+    ]
+    .groupby(['year', 'country'])[yaxis_co2_source].sum()
+    .to_frame()
+    .reset_index()
+    .sort_values(by='year')  
+    .reset_index(drop=True)
+)
+
+co2_source_bar_plot = co2_source_bar_pipeline.hvplot(kind='bar', 
+                                                     x='country', 
+                                                     y=yaxis_co2_source, 
+                                                     title='CO2 source by continent')
+co2_source_bar_plot
+
+
+#Layout using Template
+template = pn.template.FastListTemplate(
+    title='World CO2 emission dashboard', 
+    sidebar=[pn.pane.Markdown("# CO2 Emissions and Climate Change"), 
+             pn.pane.Markdown("#### Carbon dioxide emissions are the primary driver of global climate change. It’s widely recognised that to avoid the worst impacts of climate change, the world needs to urgently reduce emissions. But, how this responsibility is shared between regions, countries, and individuals has been an endless point of contention in international discussions."), 
+             pn.pane.PNG('climate_day.png', sizing_mode='scale_both'),
+             pn.pane.Markdown("## Settings"),   
+             year_slider],
+    main=[pn.Row(pn.Column(yaxis_co2, 
+                           co2_plot.panel(width=700), margin=(0,25)), 
+                 co2_table.panel(width=500)), 
+          pn.Row(pn.Column(co2_vs_gdp_scatterplot.panel(width=600), margin=(0,25)), 
+                 pn.Column(yaxis_co2_source, co2_source_bar_plot.panel(width=600)))],
+    accent_base_color="#88d8b0",
+    header_background="#88d8b0",
+)
+# template.show()
+template.servable();
